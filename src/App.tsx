8b74f6a21b95c5275377426c93540b8bddac7e81
HyukHyunCho <gurgus0189@gmail.com>
2023-02-26 23:10:23 +0900

    Feat: Drag And Drop Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { todoState } from "./atoms";
+import Board from "./Container/BoardContainer";
+import ModalContainer from "./Container/ModalContainer";
+
+const Layout = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 10px 1%;
+`;
+
+const BoardContainer = styled.div`
+  flex-basis: 23.5%;
+  height: 400px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  border-radius: 10px;
+  background-color: #e6eef4;
+`;
 
 function App() {
+  const [toDos, setToDos] = useRecoilState(todoState);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const addList = () => {
+    setModalOpen(true);
+  };
+  
+  console.log(toDos)
+
+  const onDragEnd = (info: DropResult) => {
+    const { destination, source } = info;
+    if (!destination) return;
+    if (destination?.droppableId === source.droppableId) {
+      setToDos(allBoards => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        const taskObj = boardCopy[source.index];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, taskObj);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      setToDos(allBoards => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const taskObj = sourceBoard[source.index];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, taskObj);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <button onClick={() => addList()}>AddList</button>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Wrapper>
+          {Object.keys(toDos).map(boardId => (
+            <BoardContainer key={boardId}>
+              <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
+            </BoardContainer>
+          ))}
+        </Wrapper>
+      </DragDropContext>
+      {modalOpen && (
+        <ModalContainer
+          setModalOpen={setModalOpen}
+        />
+      )}
+    </Layout>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+import { darkTheme, lightTheme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-weight: 300;
+    background-color: ${(props) => props.theme.bgColor};
+    color: #000;
+  }
+`;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
