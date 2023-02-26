187c805f1ffe9fd026951ceea5fd731ea9b72569
HyukHyunCho <gurgus0189@gmail.com>
2023-02-26 23:10:41 +0900

    Feat: theme 추가

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#EAEAEA",
+  boardColor: "#FF0000",
+  cardColor: "#000",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#fff",
+  boardColor: "#DADFE9",
+  cardColor: "#FFF",
+};
